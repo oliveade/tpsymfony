6afc5dd46864e8ce94d18ca6ac86c075a3c0df82
Olive ADE <oliveade@MacBook-Air-de-Olive.local>
2025-01-10 17:00:19 +0100

    améliorations

--- tp/src/Controller/Controller.php
+++ tp/src/Controller/Controller.php
@@ -4,7 +4,7 @@ Namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Repository\DishRepository;
 class Controller extends AbstractController
 {
     #[Route('/', name: 'app_home')]
@@ -13,9 +13,12 @@ class Controller extends AbstractController
         return $this->render('/index.html.twig');
     }
     #[Route('/test', name:'app_admin')]
-    public function maPage(): Response
+    public function maPage(DishRepository $dishRepository): Response
     {
-        return $this->render('/home.html.twig');
+        $dishes = $dishRepository->findAll();
+        return $this->render('/home.html.twig', [
+            'dishes' => $dishes,
+        ]);
     }
     #[Route('/adminPage', name:'app_admin_connection')]
     public function adminDarshboard(): Response
